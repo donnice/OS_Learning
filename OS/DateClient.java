f0c929db0d0312fae47e6fed88a857178eb2cfdb
Hai Tang <402696415@qq.com>
2018-05-29 03:18:10 -0700

    socket in java

---
+++ OS/DateClient.java
@@ -0,0 +1,24 @@
+import java.net.*;
+import java.io.*;
+
+public class DateClient {
+    public static void main(String[] args) {
+        try {
+            /* make connection to server socket */
+            Socket sock = new Socket("127.0.0.1", 6013);
+
+            InputStream in = sock.getInputStream();
+            BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+
+            /* read date from socket */
+            String line;
+            while((line = bin.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            sock.close();
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        }
+    }
+}
